3d40e77870728eb0a8124300a12aacdc7dab7178
Genki Ishibashi <b0941015@gmail.com>
2014-04-13 22:23:18 +0900

    stopコマンド追加

--- nodeCommand.js
+++ nodeCommand.js
@@ -7,42 +7,39 @@ module.exports = {
 
     judgeComand: function(data){
 	var command = 'irsend SEND_START lircd.conf ';
+	var stopCmd = 'irsend SEND_STOP lircd.conf ';
 
 	switch(data){
 	case "power":
 	    command += "power";
+	    stopCmd += "power"
 	    break;
 	case "vHigh":
 	    command += "volumeUp";
+	    stopCmd += "volumeUp";
 	    break;
 	case "vLow":
 	    command += "volumeDown";
+	    stopCmd += "volumeDown";	    
 	    break;
 	case "cUp":
 	    command += "channelUp";
+	    stopCmd += "channelUp";	    
 	    break;
 	case "cDown":
 	    command += "channelDown";
+	    stopCmd += "channelDown";
 	    break;
 	};
 
 	child_process.exec(command, function(err, stdout, stderr){
 	    console.log(stdout);
 	});
-    }
-    
-    switchOn: function(data){
-	var command = 'irsend SEND_START lircd.conf power';
-	
-	child_process.exec(command, function(err, stdout, stderr){
-	    console.log(stdout);
-	});
-    },
 
-    switchOff: function(data){
-	var command = 'irsend SEND_STOP lircd.conf power';	
-	child_process.exec(command, function(err, stdout, stderr){
-	    console.log(stdout);
-	});	
-    }	
+	setTimeout(function(){
+	    child_process.exec(stopCmd, function(err, stdout, stderr){
+		console.log(stdout);
+	    })
+	}, 1000);
+    }
 };
